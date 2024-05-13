423ba099483d58b17a13bcf3ce71216e9e62bba8
Diganta123 <diganta15-4898@diu.edu.bd>
2024-05-13 20:19:47 +0600

    update

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is the Home Page 😍")
+    return render(request, "index.html")
 
 def about(request):
     return HttpResponse("About us ? 😒")
